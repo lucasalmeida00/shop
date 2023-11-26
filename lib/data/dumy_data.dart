418b88711e1d5f78a0c7cdfb28b87cc081a184b7
Lucas Almeida <lucasalmeidasilva0@gmail.com>
2023-11-26 12:04:12 -0300

    chore: criado mock de produtos

---
+++ lib/data/dumy_data.dart
@@ -0,0 +1,36 @@
+import 'package:shop/models/product.dart';
+
+final dummyProducts = [
+  Product(
+    id: 'p1',
+    title: 'Red Shirt',
+    description: 'A red shirt - it is pretty red!',
+    price: 29.99,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+  ),
+  Product(
+    id: 'p2',
+    title: 'Trousers',
+    description: 'A nice pair of trousers.',
+    price: 59.99,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2016/03/27/19/43/fashion-1283863_1280.jpg',
+  ),
+  Product(
+    id: 'p3',
+    title: 'Yellow Scarf',
+    description: 'Warm and cozy - exactly what you need for the winter.',
+    price: 19.99,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_1280.jpg',
+  ),
+  Product(
+    id: 'p4',
+    title: 'A Pan',
+    description: 'Prepare any meal you want.',
+    price: 49.99,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2017/03/22/17/39/kitchen-2165756_1280.jpg',
+  ),
+];
