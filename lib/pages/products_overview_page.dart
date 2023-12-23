96162c021dab6de0004fc3ab9e425b894e763210
Lucas Almeida <lucasalmeidasilva0@gmail.com>
2023-12-22 23:50:07 -0300

    feat: adicionado detalhes de produto

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:shop/components/product_item.dart';
+import 'package:shop/data/dumy_data.dart';
+import 'package:shop/models/product.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  const ProductsOverviewPage({super.key});
+  final List<Product> loadedProducts = dummyProducts;
+
+  ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyShop'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ),
+        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
+        itemCount: loadedProducts.length,
+      ),
+    );
   }
 }
---
+++ lib/utils/routes.dart
@@ -0,0 +1,5 @@
+class AppRoutes {
+  static const String HOME = '/';
+  static const String PRODUCTS_LIST = '/products';
+  static const String PRODUCT_DETAIL = '/product-detail';
+}
