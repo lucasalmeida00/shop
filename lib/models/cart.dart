a8d751b8b7c1738b6628279e836b8588d709dd17
Lucas Almeida <lucasalmeidasilva0@gmail.com>
2023-12-25 15:56:35 -0300

    feat: adicionado funções para retornar total do preço do carrinho

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -5,4 +5,45 @@ class Cart with ChangeNotifier {
   final Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items => {..._items};
+
+  int get itemsCount => _items.length;
+
+  double get totalAmount {
+    double total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void addItem(CartItem cartItem) {
+    if (_items.containsKey(cartItem.productId)) {
+      _items.update(
+        cartItem.productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          productId: existingCartItem.productId,
+          name: existingCartItem.name,
+          quantity: existingCartItem.quantity + 1,
+          price: existingCartItem.price,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        cartItem.productId,
+        () => cartItem,
+      );
+    }
+    notifyListeners();
+  }
+
+  void clear() {
+    _items.clear();
+    notifyListeners();
+  }
 }
